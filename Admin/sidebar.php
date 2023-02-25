08f8f98a1088b90b76ead1f65afcdb272c5643a1
eng-omar23 <omarmohamud531@gmail.com>
2023-02-25 21:20:47 +0300

    sidebar setup

--- Admin/sidebar.php
+++ Admin/sidebar.php
@@ -1,22 +1,38 @@
 <!-- SIDEBAR -->
 <section id="sidebar">
-		<a href="#" class="brand"><i class='bx bxs-smile icon'></i> AdminSite</a>
+		<a href="#" class="brand"><i class='bx bxs-smile icon'></i>Hall booking</a>
 		<ul class="side-menu">
 			<li><a href="#" class="active"><i class='bx bxs-dashboard icon' ></i> Dashboard</a></li>
 			<li class="divider" data-text="main">Main</li>
 			<li>
-				<a href="#"><i class='bx bxs-inbox icon' ></i> Elements <i class='bx bx-chevron-right icon-right' ></i></a>
+				<a href="#"><i class='bx bxs-inbox icon' ></i>Registeration<i class='bx bx-chevron-right icon-right' ></i></a>
 				<ul class="side-dropdown">
-					<li><a href="#">Alert</a></li>
-					<li><a href="#">Badges</a></li>
-					<li><a href="#">Breadcrumbs</a></li>
-					<li><a href="#">Button</a></li>
+					<li><a href="./user/user.php">Users</a></li>
+					<li><a href="./customers/customers">Customers</a></li>
+					<li><a href="#">Halls</a></li>
+                    <li><a href="#">Facility</a></li>
+
 				</ul>
 			</li>
-			<li><a href="#"><i class='bx bxs-chart icon' ></i> Charts</a></li>
-			<li><a href="#"><i class='bx bxs-widget icon' ></i> Widgets</a></li>
-			<li class="divider" data-text="table and forms">Table and forms</li>
-			<li><a href="#"><i class='bx bx-table icon' ></i> Tables</a></li>
+            <li>
+				<a href="#"><i class='bx bxs-inbox icon' ></i>Booking<i class='bx bx-chevron-right icon-right' ></i></a>
+				<ul class="side-dropdown">
+					<li><a href="./user/user.php">booking</a></li>
+					<li><a href="./customers/customers">Payment</a></li>
+		
+
+				</ul>
+			</li>		<li>
+				<a href="#"><i class='bx bxs-inbox icon' ></i>Reports<i class='bx bx-chevron-right icon-right' ></i></a>
+				<ul class="side-dropdown">
+					<li><a href="./user/user.php">Booking</a></li>
+					<li><a href="./customers/customers">cancelledBooking</a></li>
+		
+
+				</ul>
+			</li>
+		
+			<li><a href="#"><i class='bx bx-notepad icon' ></i> Logs</a></li>
 			<li>
 				<a href="#"><i class='bx bxs-notepad icon' ></i> Forms <i class='bx bx-chevron-right icon-right' ></i></a>
 				<ul class="side-dropdown">
@@ -30,7 +46,7 @@
 		<div class="ads">
 			<div class="wrapper">
 				<a href="#" class="btn-upgrade">Upgrade</a>
-				<p>Become a <span>PRO</span> member and enjoy <span>All Features</span></p>
+		
 			</div>
 		</div>
 	</section>
